ab310eafa63737fdf0ba7bbefab8bce7ba2d5029
Luan Campos <luancamposproducoes@gmail.com>
2023-05-20 15:49:28 -0300

    create crud memories

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,13 @@
 import fastify from 'fastify'
+import cors from '@fastify/cors'
+import { memoriesRoutes } from './routes/memories'
 
 const app = fastify()
 
-app.get('/', () => {
-  return 'Hello World!'
+app.register(cors, {
+  origin: true,
 })
+app.register(memoriesRoutes)
 
 app
   .listen({
