c393f6019c5ed9ba2e8829e26393c50c5c0ac276
Luan Campos <luancamposproducoes@gmail.com>
2023-05-20 16:53:30 -0300

    github code

---
+++ web/src/app/components/Copyright.tsx
@@ -0,0 +1,24 @@
+export function Copyright() {
+  return (
+    <div className="text-sm leading-relaxed text-gray-200">
+      Feito com 💜 por{' '}
+      <a
+        target="_blank"
+        className="underline hover:text-gray-100"
+        href="https://github.com/luancamposdev"
+        rel="noreferrer"
+      >
+        Luan Campos
+      </a>{' '}
+      no NLW da{' '}
+      <a
+        target="_blank"
+        className="underline hover:text-gray-100"
+        href="http://rocketseat.com.br"
+        rel="noreferrer"
+      >
+        Rocketseat
+      </a>
+    </div>
+  )
+}
